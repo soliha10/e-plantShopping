a015f64698e5bf22afbb84d2c8f52684f47035b9
soliha10 <toxtamurodovagulshodaa@gmail.com>
2025-07-11 02:13:11 -0400

    Add function 2 components

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -255,14 +255,17 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
-const handleAddToCart = (product) => {
-dispatch(addItem(product))
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
 
-setAddedToCart((prevState) => ({
-    ...prevState,
-    [product.name] : true
-}))
-}
+        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
+            ...prevState, // Spread the previous state to retain existing entries
+            [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
+        }));
+    };
+    const calculateTotalQuantity = () => {
+ return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+  };
 
     return (
         <div>
